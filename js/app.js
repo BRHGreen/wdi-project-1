f2696a4ab61c7f962dc1d5f1b4ac9cd3d46c7632
BRHGreen <benedictgreen@gmail.com>
2016-08-20 15:16:38 +0100

    shuffling, sort of

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+
 //Create playing board which consists of a four by four grid.
 //Equal sections of a picture should be divided up into 15 of these 16 squares
 //the 15 square should be blank 
@@ -10,82 +11,96 @@
 
 $(start)
 
+var start = [1,2,3,4,5,6,7,8];
+var shuffle = []
+var plays = [];
+var elements = [
+{id: 0},
+{id: 1},
+{id: 2},
+{id: 3},
+{id: 4},
+{id: 5},
+{id: 6},
+{id: 7},
+{id: 8}
 
+
+  // var item = "<li id='element.id'>"+ element.id + "</li>"
+  // $('ul').append(item);
+
+  ]
   
-var positions = {
-  a: $("#1"),
-  b: $("#2"),
-  c: $("#3"),
-  d: $("#4"),
-  e: $("#5"),
-  f: $("#6"),
-  g: $("#7"),
-  h: $("#8"),
-  i: $("#9"),
-  j: $("#10"),
-  k: $("#11"), 
-  l: $("#12"),
-  m: $("#13"),
-  n: $("#14"),
-  o: $("#15"),
-  p: $("#x")
+
+  function start () {
+
+    createTiles()
+
+
+
+    function createTiles() {
+      $(elements).each(function(index){
+
+        $("<li id="+index+">"+index+"</li>").appendTo("ul");
+      })
+    }
+
+    function remove() {
+
+    }
+
+
+    $("#shuffle").click(shuffle)
+
+    function shuffle () {
+      $("li").remove()
+      $(elements).each(function(index){
+        index = Math.floor(Math.random()*8);
+        $("<li id="+index+">"+index+"</li>").appendTo("ul");
+      })
+    }
+
+    // function shuffle () {
+    //   $("elements").each(function {
+        
+    //   })
+    // }
+
+
+
+  // shuffle elements array
+
+  // loop over elements array once shuffled, for each element inside the array append a li to the ul container
+
+  // once inside, loop over lis and create a new array of the ids
+  // [4,5,1,2,8,7,6]
+  // current playing array
+
+  // click event on lis
+  // compare the two arrays, if they are exactly the same, WIN, if not, keep playing the game
+
+
+
+
 }
- console.log(positions)
 
 
 
-function start () {
-
-$("li").click(move);
-
-function move () {  
-    $(this).animate({left: '100px'});
-    positions.p = $("#15")
-    console.log(positions)
-}
-
-
-
-
-
-}
-
-
-
-// $(function() {
-//     $("#drag").sortable({ 
-//     });
-// });
-
-// $(function() {
-//     $( '#drag' ).sortable({
-//         stop: function () {
-//             var nbElems = ul.length;
-//             $('ul.currentposition').each(function(idx) {
-//                 $(this).val(nbElems - idx);
-//             });
-//         }
-//     });
-// });
-
-// var tile = $(this).on('click', function (){
-//   
+// function setup () {
+//   $("li").click (function (){ 
+//   $("li").each(function (){
+//     start.push($("li"));
+//     console.log(start)
+//   })
 // })
 // }
 
+// $("li").click (function(){
+//   plays.push(this);
+//   console.log(plays)
+// })
 
-//board click function:
 
 
-//level select function: 
-  // var $next = $('#next').on('click', bigger)
 
-  // var $prev = $('#prev').on('click', smaller) 
 
-  // function bigger() {
-  //   $('main').css({'height':'500px', 'width':'500px'})
-  //    }
-
-  // function smaller() {
-  //   $('main').css({'height':'300px', 'width':'300px'})
-  //   }

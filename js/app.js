1285c40224afa9f6fb66e07cacaa64d73d0b4a29
BRHGreen <benedictgreen@gmail.com>
2016-08-22 16:20:08 +0100

    Sliding squares

--- js/app.js
+++ js/app.js
@@ -1,16 +1,14 @@
 $(start)
-var start            = [0,1,2,3,4,5,6,7,"x"];
+var start           = [0,1,2,3,4,5,6,7,"x"];
 var shuffled;
 var width           = 3;
-
 var currentBoard    = []
 var newBoard        = []
 
-
-
 function start () {
   createTiles(start);
-  $("#shuffle").on("click", shuffle); 
+
+  $("#shuffle").on("click", shuffle);
   $("ul").on("click", "li", move); 
 }
 
@@ -19,8 +17,7 @@ function createTiles (array) {
   $("li").remove();
   $.each(array, function(index, value) {
     $("<li id="+value+">"+value+"</li>").appendTo($ul);
-
-  })
+  });
 }
 
 function shuffle() {
@@ -35,73 +32,78 @@ function shuffle() {
   }
 
   createTiles(shuffled);
-
-  $(shuffled).each(function(index, value){
-    currentBoard.push(value)
-    // console.log(currentBoard)
-  })
-
 } 
 
-function move () {
-  if (!shuffled) return + alert("You need to shuffle"); 
-  var $square  = $(this);
-  var $lis     = $("li");
-  var lisArray = [].slice.call($lis);
-  var index    = lisArray.indexOf(this);
+function move() {
+  if (!shuffled) return alert("You need to shuffle"); 
+  var $square       = $(this);
+  var $x            = $("#x");
+  var $lis          = $("li");
+  var lisArray      = [].slice.call($lis);
+  var id            = parseInt($square.attr("id"));
+  var index         = shuffled.indexOf(id);
+  var currentSquare = shuffled[index];
+  var newIndex, newSquare;
+
+  console.log("Index of lis", index);
+  console.log($lis[index]);
+  console.log("Number in shuffled array", shuffled[index]);
+  if ($lis[index] === "x") return alert("Don't click on x");
 
   $square.css("background", "red");
 
+  var prevLeft  = parseInt($square.css("left"));
+  var prevTop   = parseInt($square.css("top"));
+  var prevXLeft = parseInt($("#x").css("left"));
+  var prevXTop  = parseInt($("#x").css("top"));
+
   if (shuffled[index-width] === "x") {
-    $(this).animate({
-      top: '-102px'
+    $square.animate({
+      top: prevTop - 102 + "px"
     })
-    $("#x").animate({
-      bottom: '-102px'
+    $x.animate({
+      top: prevXTop + 102 + "px"
     })
-    $(currentBoard).each(function(value, index){
-      if (value
-    return console.log("up");
-  })
-  }
-
-  else if (shuffled[index-1]     === "x") { 
-    $(this).animate({
-      left: '-102px'
+    newIndex = index-width;
+  } else if (shuffled[index+width] === "x") {
+    $square.animate({
+      top: prevTop + 102 + "px"
     })
-    $("#x").animate({
-      right: '-102px'
+    $x.animate({
+      top: prevXTop - 102 + "px"
     }) 
-    return console.log("left");
-  }
-
-  else if  (shuffled[index+1]     === "x") {
-    $(this).animate({
-      left: '102px'
+    newIndex = index+width;
+  } else if (shuffled[index-1] === "x") { 
+    $square.animate({
+      left: prevLeft - 102 + "px"
     })
-    $("#x").animate({
-      left: '-102px'
+    $x.animate({
+      left: prevXLeft + 102 + "px"
     }) 
-    return console.log("right");
-  }
-
-  else if (shuffled[index+width] === "x") {
-    $(this).animate({
-      bottom: '-102px'
+    newIndex = index-1;
+  } else if (shuffled[index+1] === "x") {
+    $square.animate({
+      left: prevLeft + 102 + "px"
     })
-    $("#x").animate({
-      top: '-102px'
+    $x.animate({
+      left: prevXLeft - 102 + "px"
     }) 
-    return console.log("down");
-  }
+    newIndex = index+1;
+  } 
 
-  else {
-    console.log('no')
-  }  
+  newSquare          = shuffled[newIndex];
+  shuffled[newIndex] = currentSquare;
+  shuffled[index]    = newSquare;
 }
 
-
-
+function displayBoard(array){
+  var board = "\n";
+  var i,j,temparray,chunk = 3;
+  for (i=0,j=array.length; i<j; i+=chunk) {
+    board += array.slice(i,i+chunk).toString()+"\n";
+  }
+  return board;
+}
 
 
 

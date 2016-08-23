9aeabaadad6ccbe113a8cf9080eb8a320045ceb9
BRHGreen <benedictgreen@gmail.com>
2016-08-23 17:05:39 +0100

    choppy picture no worky

--- js/app.js
+++ js/app.js
@@ -9,8 +9,6 @@ var newBoard        = []
 
 function start () {
 
-  // $('li').prepend('<img id="theImg" src="/Users/Mac/generalAssembly/Development/wdi-project-1/images/escherLizardsAgain.jpg" />')
-
   createTiles(start);
 
   $("#shuffle").on("click", shuffle);
@@ -21,38 +19,32 @@ function createTiles (array) {
   var $ul = $("ul");
   $("li").remove();
   $.each(array, function(index, value) {
-    if (value !== "x") {
-    $("<li id="+value+"><img src='./images/escherLizardsAgain.jpg'></li>").appendTo($ul);
-  }
+    $("<li id=id"+value+"><img src='./images/text.jpg'></li>").appendTo($ul);
   });
 }
+
 
 function shuffle() {
   var randomTileIndex, lastTile, numberOfTiles; 
   shuffled = start.slice(0);
-
   for (numberOfTiles = shuffled.length; numberOfTiles; numberOfTiles-- ) {
     randomTileIndex             = Math.floor(Math.random() * numberOfTiles);
     lastTile                    = shuffled[numberOfTiles - 1];
     shuffled[numberOfTiles -1]  = shuffled[randomTileIndex]
     shuffled[randomTileIndex]   = lastTile;
+
   }
 
   createTiles(shuffled);
 } 
 
 function move() {
-  if (!shuffled) return alert("You need to shuffle"); 
-  // var playedTile = (this.id)
-  // $(shuffled).each(function (index, value){
-  //   if(value === parseInt(playedTile)) {
-  //     var playedTileIndex = index
-  //     if ((playedTileIndex === 2 || playedTileIndex === 5) && (playedTileIndex+1 === 3 || playedTileIndex+1 === 6)) {
-  //       alert("no");
-  //     }
+  $(shuffled).each(function(index, value){
+    console.log(index, value)
 
-  //   }
-  // })
+  })
+
+  if (!shuffled) return alert("You need to shuffle"); 
 
   
   var $square       = $(this);
@@ -63,13 +55,12 @@ function move() {
   var index         = shuffled.indexOf(id);
   var currentSquare = shuffled[index];
   var newIndex, newSquare;
+  console.log(index)
 
 
   if ($lis[index] === "x") return alert("Don't click on x");
   if ((shuffled[index+1] === "x") && (index === 2 || index === 5)) return alert ("Not ok") 
   if ((shuffled[index-1] === "x") && (index === 3 || index === 6)) return alert ("Not Aok") 
-
-
 
   var prevLeft  = parseInt($square.css("left"));
   var prevTop   = parseInt($square.css("top"));
@@ -124,25 +115,3 @@ function displayBoard(array){
   }
   return board;
 }
-
-
-
-/*
-   X+1    Y+1       N+1   
-    1     2,4                     3/3,5,6,7,8,9     -can't play if the square is greater than x index +3
-    2     1,3,5                   4/6,7,8,9         -can't play if the square === x index + 2
-    3     2,6       1,4,6         5/7,8,9
-    4     1,5,7     2,3,          6/8,9     
-    5     2,4,6,8   1,3,          7/9
-    6     3,5,9     1,2,4,7,      8/
-    7     4,8       1,2,3,5,6,    9/
-    8     5,7,9     1,2,3,4,6
-    9     6,8       1,2,3,4,7
-    */
-
-
-
-
-
-
-

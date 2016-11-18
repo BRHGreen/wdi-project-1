57db3f2186b589204d45c4e2e9d0f86ed85fe4e4
BRHGreen <benedictgreen@gmail.com>
2016-11-18 21:00:27 +0000

    fix jshints redlines

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-$(start)
+$(start);
 var start           = [0,1,2,3,4,5,6,7,"x"];
 var shuffled;
 var width           = 3;
@@ -6,11 +6,11 @@ function start () {
   createTiles(start);
 
   $("span").on("click", shuffle);
-  $("ul").on("click", "li", move); 
+  $("ul").on("click", "li", move);
   $(".popup").on("click", function() {
-    $(this).toggleClass("showPopup")
-    console.log(this)
-  })
+    $(this).toggleClass("showPopup");
+    console.log(this);
+  });
 
 }
 
@@ -23,35 +23,35 @@ function createTiles (array) {
 }
 
 function shuffle() {
-  var randomTileIndex, lastTile, numberOfTiles; 
+  var randomTileIndex, lastTile, numberOfTiles;
   shuffled = start.slice(0);
   for (numberOfTiles = shuffled.length; numberOfTiles; numberOfTiles-- ) {
     randomTileIndex             = Math.floor(Math.random() * numberOfTiles);
     lastTile                    = shuffled[numberOfTiles - 1];
-    shuffled[numberOfTiles -1]  = shuffled[randomTileIndex]
+    shuffled[numberOfTiles -1]  = shuffled[randomTileIndex];
     shuffled[randomTileIndex]   = lastTile;
   }
 
   createTiles(shuffled);
 
-  $("#idx img").css("background-color", "white")
-  $("#idx img").css("z-index", "-1")
-} 
+  $("#idx img").css("background-color", "white");
+  $("#idx img").css("z-index", "-1");
+}
 
 function move() {
-  if (!shuffled) return alert("You need to shuffle"); 
-  
+  if (!shuffled) return alert("You need to shuffle");
+
   var $square       = $(this);
   var $x            = $("#idx");
   var $lis          = $("li");
   var lisArray      = [].slice.call($lis);
-  var id            = parseInt($square.attr("id").replace("id", ""))
+  var id            = parseInt($square.attr("id").replace("id", ""));
   var index         = shuffled.indexOf(id);
   var currentSquare = shuffled[index];
   var newIndex, newSquare;
 
   if ($lis[index] === "xid") return;
-  
+
   var w = width;
   var possibleIndexes = [index-w, index+1, index+w, index-1];
 
@@ -78,34 +78,34 @@ function move() {
   if (shuffled[index-width] === "x")  {
     $square.animate({
       top: prevTop - 100 + "px"
-    })
+    });
     $x.animate({
       top: prevXTop + 100 + "px"
-    })
+    });
     newIndex = index-width;
   } else if (shuffled[index+width] === "x") {
     $square.animate({
       top: prevTop + 100 + "px"
-    })
+    });
     $x.animate({
       top: prevXTop - 100 + "px"
-    }) 
+    });
     newIndex = index+width;
-  } else if (shuffled[index-1] === "x") { 
+  } else if (shuffled[index-1] === "x") {
     $square.animate({
       left: prevLeft - 100 + "px"
-    })
+    });
     $x.animate({
       left: prevXLeft + 100 + "px"
-    }) 
+    });
     newIndex = index-1;
   } else if (shuffled[index+1] === "x") {
     $square.animate({
       left: prevLeft + 100 + "px"
-    })
+    });
     $x.animate({
       left: prevXLeft - 100 + "px"
-    }) 
+    });
     newIndex = index+1;
   }
 
@@ -113,6 +113,3 @@ function move() {
   shuffled[newIndex] = currentSquare;
   shuffled[index]    = newSquare;
 }
-
-
-
